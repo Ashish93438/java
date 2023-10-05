cd52eac93f93271684275bac8591784c7331c98a
Ashish93438 <147086795+Ashish93438@users.noreply.github.com>
2023-10-05 22:14:13 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class array {
+    public static void main(String args[])
+    {
+        // int marks[] = new int[5];
+        // marks[0] = 45;
+        // marks[1] = 76;
+        // marks[2] = 87;
+        // marks[3] = 76;
+        // marks[4] = 87;
+        // System.out.println(marks[0]);
+
+        // int marks[] = {90,78,45,37,25};
+        // for(int i=0;i<marks.length; i++)
+        // {
+        // System.out.println(marks[i]);
+        // }
+
+        // <---  decrement for loop  -->
+        // int marks[] = {90,78,45,37,25};
+        // for(int i=marks.length-1;i>=0; i--)
+        // {
+        // System.out.println(marks[i]);
+        // }
+          
+       // <---- for each loop in display  -->//
+
+        // int marks[] = {90,78,45,37,25};
+        // for(int element : marks)
+        // {
+        // System.out.println(element);
+        // }
+
+    }
+}
